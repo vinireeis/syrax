f2eeec11f8c806bff6a7705d75b0604520ca6b17
Vinicius Reis <v_reis@outlook.com.br>
2024-10-26 23:07:42 -0300

    fix: db_init changes

--- init_db/db_script.sql
+++ init_db/db_script.sql
@@ -1,19 +1,18 @@
 CREATE TABLE Accounts (
-    account_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    balance DECIMAL(15, 2) NOT NULL CHECK (balance >= 0)
+    account_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    balance_end_day DECIMAL(15, 2) NOT NULL DEFAULT 0
 );
 
 CREATE TABLE Transactions (
-    transaction_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    beneficiary_account UUID REFERENCES Accounts(account_uuid),
+    transaction_id SERIAL PRIMARY KEY,
+    account_id UUID REFERENCES Accounts(account_id),
     amount DECIMAL(15, 2) NOT NULL CHECK (amount > 0),
-    cash_flow VARCHAR(10) CHECK (cash_flow IN ('cash_in', 'cash_out')) NOT NULL,
+    transaction_datetime TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     operation VARCHAR(10) CHECK (operation IN ('deposit', 'transfer', 'withdraw')) NOT NULL,
-    transaction_timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
-    origin_account UUID REFERENCES Accounts(account_uuid),
-    description TEXT
+    cash_flow VARCHAR(10) CHECK (cash_flow IN ('cash_in', 'cash_out')) NOT NULL,
+    reference_id UUID,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_beneficiary_account ON Transactions (beneficiary_account);
-CREATE INDEX idx_origin_account ON Transactions (origin_account);
+CREATE INDEX idx_account_id ON Transactions (account_id);
+CREATE INDEX idx_reference_id ON Transactions (reference_id);
